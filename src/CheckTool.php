d58a899e925651453b1993c90f290d0a7ab5e369
kangxuan <895560759@qq.com>
2024-11-22 10:48:48 +0800

    update CheckTool

--- src/CheckTool.php
+++ src/CheckTool.php
@@ -11,16 +11,28 @@ declare(strict_types = 1);
 namespace Shanla\Tools;
 
 
+use DateTime;
+
 class CheckTool
 {
     /**
      * 校验手机号格式
      * @param string $mobile
+     * @param string $country
      * @return bool
      */
-    public static function checkMobile(string $mobile) : bool
+    public static function checkMobile(string $mobile, string $country = 'CN') : bool
     {
-        if (preg_match('/^1[3|4|5|6|7|8|9]\d{9}$/', $mobile)) {
+        $patterns = [
+            'CN' => '/^1[3-9]\d{9}$/', // 中国
+            'US' => '/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/', // 美国
+            'UK' => '/^07\d{9}$/', // 英国
+            'INTL' => '/^\+?[1-9]\d{1,14}$/', // 国际
+        ];
+        if (!isset($patterns[$country])) {
+            return false;
+        }
+        if (preg_match($patterns[$country], $mobile)) {
             return true;
         }
         return false;
@@ -33,20 +45,26 @@ class CheckTool
      */
     public static function checkEmail(string $email) : bool
     {
-        if (preg_match('/^[_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,4}$/', $email)) {
-            return true;
-        }
-        return false;
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
     /**
      * 校验邮编格式
      * @param string $postalCode
+     * @param string $country
      * @return bool
      */
-    public static function checkPostalCode(string $postalCode) : bool
+    public static function checkPostalCode(string $postalCode, string $country = 'CN') : bool
     {
-        if (preg_match("/^[1-9]\d{5}$/", $postalCode)) {
+        $patterns = [
+            'CN' => '/^\d{6}$/', // 中国
+            'US' => '/^\d{5}(-\d{4})?$/', // 美国
+            'UK' => '/^[A-Z]{1,2}\d[A-Z\d]? \d[A-Z]{2}$/i', // 英国
+        ];
+        if (!isset($patterns[$country])) {
+            return false;
+        }
+        if (preg_match($patterns[$country], $postalCode)) {
             return true;
         }
         return false;
@@ -59,55 +77,72 @@ class CheckTool
      */
     public static function checkIpAddress(string $ipAddress) : bool
     {
-        if (preg_match("/^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])" .
-            "(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/", $ipAddress)) {
-            return true;
-        }
-        return false;
+        return filter_var($ipAddress, FILTER_VALIDATE_IP) != false;
     }
 
     /**
-     * 校验生日格式
-     * @param string $birth
+     * 校验日期有效性
+     * @param string $date
      * @param string $format
      * @return bool
      */
-    public static function checkBirth(string $birth, string $format = 'Y-m-d') : bool
+    public static function checkDate(string $date, string $format = 'Y-m-d') : bool
     {
-        $unixTime = strtotime($birth);
-        if (!$unixTime) {
-            return false;
-        }
-
-        // 校验生日的格式是否正确
-        if (date($format, $unixTime) == $birth) {
-            return true;
-        } else {
-            return false;
-        }
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) === $date;
     }
 
     /**
-     * 校验中国身份证号格式
+     * 校验中国大陆身份证号格式
      * @param string $idCard
      * @return bool
      */
     public static function checkChineseIdCardNo(string $idCard) : bool
     {
-        if (preg_match('/(^([\d]{15}|[\d]{18}|[\d]{17}x)$)/', $idCard)) {
-            return true;
+        // 检查长度和基本格式
+        if (!preg_match('/^\d{15}$|^\d{17}(\d|X)$/i', $idCard)) {
+            return false;
         }
-        return false;
+        // 转换为18位（如果是 15 位）
+        if (strlen($idCard) === 15) {
+            $idCard = substr($idCard, 0, 6) . '19' . substr($idCard, 6, 9);
+            $idCard .= self::calculateCheckDigit($idCard);
+        }
+        // 校验出生日期
+        $birthDate = substr($idCard, 6, 8);
+        if (!self::checkDate($birthDate, 'Ymd')) {
+            return false;
+        }
+        // 校验校验码
+        return strtoupper(substr($idCard, 17, 1)) === self::calculateCheckDigit($idCard);
     }
 
     /**
-     * 校验是否是中文
+     * 计算身份证的校验码
+     * @param string $idCard
+     * @return string
+     */
+    private static function calculateCheckDigit(string $idCard) : string
+    {
+        $weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+        $checkDigits = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+        $sum = 0;
+
+        for ($i = 0; $i < 17; $i++) {
+            $sum += intval($idCard[$i]) * $weights[$i];
+        }
+
+        return $checkDigits[$sum % 11];
+    }
+
+    /**
+     * 校验包含中文
      * @param string $str
      * @return bool
      */
-    public static function checkChinese(string $str) : bool
+    public static function checkContainsChinese(string $str) : bool
     {
-        if(preg_match("/[\x{4e00}-\x{9fa5}]+/u", $str)) {
+        if(preg_match("//[\x{4e00}-\x{9fa5}]/u", $str)) {
             return true;
         }
         return false;
@@ -130,28 +165,14 @@ class CheckTool
      */
     public static function checkAgeAdult(string $birthday) : bool
     {
-        $toDateArr = explode('-', $birthday);
-        if(count($toDateArr) != 3){
-            return false;
+        $birthDateObj = DateTime::createFromFormat('Y-m-d', $birthday);
+        if (!$birthDateObj) {
+            return false; // 无效的日期格式
         }
-        if($toDateArr[0] < 1 || $toDateArr[1] < 1 || $toDateArr[2] < 1){
-            return false;
-        }
-        list($nowYear,$nowMonth,$nowDay) = explode('-', date('Y-m-d'));
-        list($toYear,$toMonth,$toDay) = $toDateArr;
-        if($nowYear - $toYear < 18){
-            return false;
-        }elseif($nowYear - $toYear ==18){
-            //同一年-判断月-日
-            if($nowMonth < $toMonth){
-                return false;
-            }elseif($nowMonth -$toMonth == 0){
-                //同一月判断日
-                if($nowDay < $toDay){
-                    return false;
-                }
-            }
-        }
-        return true;
+
+        $today = new DateTime(); // 当前日期
+        $age = $today->diff($birthDateObj)->y; // 计算年龄
+
+        return $age >= 18; // 判断是否成年
     }
 }
