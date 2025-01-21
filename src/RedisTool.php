6311401fb5acaea3db6aa9e6d71d2841a0628b9b
kangxuan <895560759@qq.com>
2025-01-21 15:30:42 +0800

    add redisTool

--- src/RedisTool.php
+++ src/RedisTool.php
@@ -15,8 +15,8 @@ class RedisTool
         if (self::$client === null) {
             self::$client = new Client([
                 'scheme' => 'tcp',
-                'host' => '127.0.0.1',
-                'port' => '6379'
+                'host'   => '127.0.0.1',
+                'port'   => '6379'
             ]);
         }
     }
@@ -196,5 +196,162 @@ class RedisTool
     public static function rPopLPush(string $key1, string $key2) : ?string
     {
         return self::$client->rpoplpush($key1, $key2);
+    }
+
+    /**
+     * 设置下标为index的值为value
+     * @param string $key
+     * @param int $index
+     * @param string $value
+     * @return mixed
+     */
+    public static function lSet(string $key, int $index, string $value) : mixed
+    {
+        return self::$client->lset($key, $index, $value);
+    }
+
+    /**
+     * 在从左到右第一个已有值前/后插入一个新值
+     * @param string $key
+     * @param string $value
+     * @param string $newValue
+     * @param string $where
+     * @return int
+     */
+    public static function lInsert(string $key, string $value, string $newValue, string $where = 'before') : int
+    {
+        return self::$client->linsert($key, $where, $value, $newValue);
+    }
+
+    /**
+     * 设置hash的单个字段值
+     * @param string $key
+     * @param string $field
+     * @param string $value
+     * @return int
+     */
+    public static function hSet(string $key, string $field, string $value) : int
+    {
+        return self::$client->hset($key, $field, $value);
+    }
+
+    /**
+     * 获取hash单个字段值
+     * @param string $key
+     * @param string $field
+     * @return string|null
+     */
+    public static function hGet(string $key, string $field) : ?string
+    {
+        return self::$client->hget($key, $field);
+    }
+
+    /**
+     * 设置hash多个字段值
+     * @param string $key
+     * @param array $map
+     * @return mixed
+     */
+    public static function hMSet(string $key, array $map) : mixed
+    {
+        return self::$client->hmset($key, $map);
+    }
+
+    /**
+     * 获取hash多个字段值
+     * @param string $key
+     * @param array $fields
+     * @return array
+     */
+    public static function hMGet(string $key, array $fields) : array
+    {
+        return self::$client->hmget($key, $fields);
+    }
+
+    /**
+     * 获取hash的所有字段值
+     * @param string $key
+     * @return array
+     */
+    public static function hGetAll(string $key) : array
+    {
+        return self::$client->hgetall($key);
+    }
+
+    /**
+     * 删除hash的多个或一个字段
+     * @param string $key
+     * @param array $fields
+     * @return int
+     */
+    public static function hDel(string $key, array $fields) : int
+    {
+        return self::$client->hdel($key, $fields);
+    }
+
+    /**
+     * 获取hash元素个数
+     * @param string $key
+     * @return int
+     */
+    public static function hLen(string $key) : int
+    {
+        return self::$client->hlen($key);
+    }
+
+    /**
+     * 获取hash的所有字段
+     * @param string $key
+     * @return array
+     */
+    public static function hKeys(string $key) : array
+    {
+        return self::$client->hkeys($key);
+    }
+
+    /**
+     * 获取hash的所有字段值
+     * @param string $key
+     * @return array
+     */
+    public static function hValues(string $key) : array
+    {
+        return self::$client->hvals($key);
+    }
+
+    /**
+     * hash某一个字段自增
+     * @param string $key
+     * @param string $field
+     * @param int $step
+     * @return int
+     */
+    public static function hIncrBy(string $key, string $field, int $step = 1) : int
+    {
+        return self::$client->hincrby($key, $field, $step);
+    }
+
+    /**
+     * hash某一个字段按照float自增
+     * @param string $key
+     * @param string $field
+     * @param int|float $step
+     * @return string
+     */
+    public static function hIncrByFloat(string $key, string $field, int|float $step = 1) : string
+    {
+        return self::$client->hincrbyfloat($key, $field, $step);
+    }
+
+    /**
+     * hash设置某个字段的值，成功返回1，否则返回0
+     * @param string $key
+     * @param string $field
+     * @param string $value
+     * @return int
+     */
+    public static function hSetNx(string $key, string $field, string $value) : int
+    {
+        return self::$client->hsetnx($key, $field, $value);
     }
 }
