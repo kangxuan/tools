a4f97679b8bc68c7d14f27175dcaa564567f9107
kangxuan <895560759@qq.com>
2025-07-02 10:00:17 +0800

    Revert "Revert "update RedisTool""
    This reverts commit 54b76dcdadb6b95183d47ea4ad406ca5d60aac70.

--- src/RedisTool.php
+++ src/RedisTool.php
@@ -3,36 +3,54 @@ declare(strict_types = 1);
 
 namespace Shanla\Tools;
 
-
 use Predis\Client;
+use Predis\Response\Status;
 
 class RedisTool
 {
-    private static Client $client;
+    private static ?Client $client = null;
 
-    public function __construct()
+    private static function getClient(): Client
     {
         if (self::$client === null) {
             self::$client = new Client([
                 'scheme' => 'tcp',
                 'host'   => '127.0.0.1',
-                'port'   => '6379'
+                'port'   => '6379',
+                'password' => '12345678'
             ]);
         }
+        return self::$client;
+    }
+
+    public static function __callStatic(string $name, array $arguments)
+    {
+        $client = self::getClient();
+        $result = $client->$name(...$arguments);
+        
+        // 处理 Predis\Response\Status 返回值
+        if ($result instanceof Status) {
+            return $result->getPayload() === 'OK';
+        }
+        
+        return $result;
     }
 
     /**
      * 设置键值对
      * @param string $key
-     * @param mixed $value
+     * @param string $value
      * @param int $ttl
      */
-    public static function set(string $key, mixed $value, int $ttl = 0) : void
+    public static function set(string $key, string $value, ?int $ttl = null): bool
     {
-        self::$client->set($key, $value);
-        if ($ttl > 0) {
-            self::$client->expire($key, $ttl);
+        $client = self::getClient();
+        if ($ttl !== null) {
+            $result = $client->setex($key, $ttl, $value);
+        } else {
+            $result = $client->set($key, $value);
         }
+        return $result instanceof Status ? $result->getPayload() === 'OK' : (bool)$result;
     }
 
     /**
@@ -40,30 +58,34 @@ class RedisTool
      * @param string $key
      * @return string|null
      */
-    public static function get(string $key) : ?string
+    public static function get(string $key): ?string
     {
-        return self::$client->get($key);
+        $client = self::getClient();
+        $result = $client->get($key);
+        return $result === null ? null : (string)$result;
     }
 
     /**
      * 删除
      * @param string $key
-     * @return int
+     * @return bool
      */
-    public static function del(string $key) : int
+    public static function del(string $key): bool
     {
-        return self::$client->del($key);
+        $client = self::getClient();
+        return (bool)$client->del($key);
     }
 
     /**
      * 设置过期时间
      * @param string $key
      * @param int $ttl
-     * @return int
+     * @return bool
      */
-    public static function expire(string $key, int $ttl) : int
+    public static function expire(string $key, int $ttl): bool
     {
-        return self::$client->expire($key, $ttl);
+        $client = self::getClient();
+        return (bool)$client->expire($key, $ttl);
     }
 
     /**
@@ -71,53 +93,58 @@ class RedisTool
      * @param string $key
      * @return int
      */
-    public static function ttl(string $key) : int
+    public static function ttl(string $key): int
     {
-        return self::$client->ttl($key);
+        $client = self::getClient();
+        return (int)$client->ttl($key);
     }
 
     /**
      * 按步长自增
      * @param string $key
-     * @param int $step
+     * @param int $increment
      * @return int
      */
-    public static function incrBy(string $key, int $step = 1) : int
+    public static function incrBy(string $key, int $increment = 1): int
     {
-        return self::$client->incrby($key, $step);
+        $client = self::getClient();
+        return (int)$client->incrby($key, $increment);
     }
 
     /**
      * 按步长自减
      * @param string $key
-     * @param int $step
+     * @param int $decrement
      * @return int
      */
-    public static function decrBy(string $key, int $step = 1) : int
+    public static function decrBy(string $key, int $decrement = 1): int
     {
-        return self::$client->decrby($key, $step);
+        $client = self::getClient();
+        return (int)$client->decrby($key, $decrement);
     }
 
     /**
      * 从左边加入列表
      * @param string $key
-     * @param array $values
+     * @param string $value
      * @return int
      */
-    public static function lPush(string $key, array $values) : int
+    public static function lPush(string $key, string|array $value): int
     {
-        return self::$client->lpush($key, $values);
+        $client = self::getClient();
+        return (int)$client->lpush($key, $value);
     }
 
     /**
      * 从右边加入列表
      * @param string $key
-     * @param array $values
+     * @param string $value
      * @return int
      */
-    public static function rPush(string $key, array $values) : int
+    public static function rPush(string $key, string|array $value): int
     {
-        return self::$client->rpush($key, $values);
+        $client = self::getClient();
+        return (int)$client->rpush($key, $value);
     }
 
     /**
@@ -125,9 +152,11 @@ class RedisTool
      * @param string $key
      * @return string|null
      */
-    public static function lPop(string $key) : ?string
+    public static function lPop(string $key): ?string
     {
-        return self::$client->lpop($key);
+        $client = self::getClient();
+        $result = $client->lpop($key);
+        return $result === null ? null : (string)$result;
     }
 
     /**
@@ -135,9 +164,11 @@ class RedisTool
      * @param string $key
      * @return string|null
      */
-    public static function rPop(string $key) : ?string
+    public static function rPop(string $key): ?string
     {
-        return self::$client->rpop($key);
+        $client = self::getClient();
+        $result = $client->rpop($key);
+        return $result === null ? null : (string)$result;
     }
 
     /**
@@ -147,32 +178,21 @@ class RedisTool
      * @param int $end
      * @return array
      */
-    public static function lRange(string $key, int $start = 0, int $end = -1) : array
+    public static function lRange(string $key, int $start = 0, int $end = -1): array
     {
-        return self::$client->lrange($key, $start, $end);
+        $client = self::getClient();
+        return array_map('strval', $client->lrange($key, $start, $end));
     }
 
     /**
-     * 按照索引下标获取列表元素
+     * 获取列表长度
      * @param string $key
-     * @param int $index
-     * @return string|null
-     */
-    public static function lIndex(string $key, int $index) : ?string
-    {
-        return self::$client->lindex($key, $index);
-    }
-
-    /**
-     * 从左边开始删除N个等于value的元素
-     * @param string $key
-     * @param int $count
-     * @param string $value
      * @return int
      */
-    public static function lRem(string $key, int $count, string $value) : int
+    public static function lLen(string $key): int
     {
-        return self::$client->lrem($key, $count, $value);
+        $client = self::getClient();
+        return (int)$client->llen($key);
     }
 
     /**
@@ -180,22 +200,12 @@ class RedisTool
      * @param string $key
      * @param int $start
      * @param int $end
-     * @return mixed
+     * @return bool
      */
-    public static function lTrim(string $key, int $start = 0, int $end = -1) : mixed
+    public static function lTrim(string $key, int $start, int $end): bool
     {
-        return self::$client->ltrim($key, $start, $end);
-    }
-
-    /**
-     * 从源list右边pop一个元素到目标list的左边，如果源list和目标list是同一个，就将list最左边的元素移动到最右边
-     * @param string $key1
-     * @param string $key2
-     * @return string|null
-     */
-    public static function rPopLPush(string $key1, string $key2) : ?string
-    {
-        return self::$client->rpoplpush($key1, $key2);
+        $client = self::getClient();
+        return (bool)$client->ltrim($key, $start, $end);
     }
 
     /**
@@ -203,24 +213,65 @@ class RedisTool
      * @param string $key
      * @param int $index
      * @param string $value
-     * @return mixed
+     * @return bool
      */
-    public static function lSet(string $key, int $index, string $value) : mixed
+    public static function lSet(string $key, int $index, string $value): bool
     {
-        return self::$client->lset($key, $index, $value);
+        $client = self::getClient();
+        return (bool)$client->lset($key, $index, $value);
     }
 
     /**
-     * 在从左到右第一个已有值前/后插入一个新值
+     * 获取列表中下标为index的值
+     * @param string $key
+     * @param int $index
+     * @return string|null
+     */
+    public static function lIndex(string $key, int $index): ?string
+    {
+        $client = self::getClient();
+        $result = $client->lindex($key, $index);
+        return $result === null ? null : (string)$result;
+    }
+
+    /**
+     * 删除列表中count个值为value的元素
+     * @param string $key
+     * @param int $count
+     * @param string $value
+     * @return int
+     */
+    public static function lRem(string $key, int $count, string $value): int
+    {
+        $client = self::getClient();
+        return (int)$client->lrem($key, $count, $value);
+    }
+
+    /**
+     * 从右边弹出一个元素并将其加入到另一个列表的左边
+     * @param string $key1
+     * @param string $key2
+     * @return string|null
+     */
+    public static function rPopLPush(string $key1, string $key2): ?string
+    {
+        $client = self::getClient();
+        $result = $client->rpoplpush($key1, $key2);
+        return $result === null ? null : (string)$result;
+    }
+
+    /**
+     * 在列表中插入一个元素
      * @param string $key
      * @param string $value
      * @param string $newValue
      * @param string $where
      * @return int
      */
-    public static function lInsert(string $key, string $value, string $newValue, string $where = 'before') : int
+    public static function lInsert(string $key, string $value, string $newValue, string $where = 'before'): int
     {
-        return self::$client->linsert($key, $where, $value, $newValue);
+        $client = self::getClient();
+        return (int)$client->linsert($key, $where, $value, $newValue);
     }
 
     /**
@@ -228,11 +279,12 @@ class RedisTool
      * @param string $key
      * @param string $field
      * @param string $value
-     * @return int
+     * @return bool
      */
-    public static function hSet(string $key, string $field, string $value) : int
+    public static function hSet(string $key, string $field, string $value): bool
     {
-        return self::$client->hset($key, $field, $value);
+        $client = self::getClient();
+        return (bool)$client->hset($key, $field, $value);
     }
 
     /**
@@ -241,20 +293,80 @@ class RedisTool
      * @param string $field
      * @return string|null
      */
-    public static function hGet(string $key, string $field) : ?string
+    public static function hGet(string $key, string $field): ?string
     {
-        return self::$client->hget($key, $field);
+        $client = self::getClient();
+        $result = $client->hget($key, $field);
+        return $result === null ? null : (string)$result;
+    }
+
+    /**
+     * 删除hash的多个或一个字段
+     * @param string $key
+     * @param string $field
+     * @return bool
+     */
+    public static function hDel(string $key, string|array $field): bool
+    {
+        $client = self::getClient();
+        return (bool)$client->hdel($key, $field);
+    }
+
+    /**
+     * 获取hash元素个数
+     * @param string $key
+     * @return int
+     */
+    public static function hLen(string $key): int
+    {
+        $client = self::getClient();
+        return (int)$client->hlen($key);
+    }
+
+    /**
+     * 获取hash的所有字段
+     * @param string $key
+     * @return array
+     */
+    public static function hKeys(string $key): array
+    {
+        $client = self::getClient();
+        return array_map('strval', $client->hkeys($key));
+    }
+
+    /**
+     * 获取hash的所有字段值
+     * @param string $key
+     * @return array
+     */
+    public static function hVals(string $key): array
+    {
+        $client = self::getClient();
+        return array_map('strval', $client->hvals($key));
+    }
+
+    /**
+     * 获取hash的所有字段
+     * @param string $key
+     * @return array
+     */
+    public static function hGetAll(string $key): array
+    {
+        $client = self::getClient();
+        $result = $client->hgetall($key);
+        return array_map('strval', $result);
     }
 
     /**
      * 设置hash多个字段值
      * @param string $key
-     * @param array $map
-     * @return mixed
+     * @param array $hash
+     * @return bool
      */
-    public static function hMSet(string $key, array $map) : mixed
+    public static function hMSet(string $key, array $hash): bool
     {
-        return self::$client->hmset($key, $map);
+        $client = self::getClient();
+        return (bool)$client->hmset($key, $hash);
     }
 
     /**
@@ -263,95 +375,63 @@ class RedisTool
      * @param array $fields
      * @return array
      */
-    public static function hMGet(string $key, array $fields) : array
+    public static function hMGet(string $key, array $fields): array
     {
-        return self::$client->hmget($key, $fields);
+        $client = self::getClient();
+        $result = $client->hmget($key, $fields);
+        return array_map(function($value) {
+            return $value === null ? null : (string)$value;
+        }, $result);
     }
 
     /**
-     * 获取hash的所有字段值
-     * @param string $key
-     * @return array
-     */
-    public static function hGetAll(string $key) : array
-    {
-        return self::$client->hgetall($key);
-    }
-
-    /**
-     * 删除hash的多个或一个字段
-     * @param string $key
-     * @param array $fields
-     * @return int
-     */
-    public static function hDel(string $key, array $fields) : int
-    {
-        return self::$client->hdel($key, $fields);
-    }
-
-    /**
-     * 获取hash元素个数
-     * @param string $key
-     * @return int
-     */
-    public static function hLen(string $key) : int
-    {
-        return self::$client->hlen($key);
-    }
-
-    /**
-     * 获取hash的所有字段
-     * @param string $key
-     * @return array
-     */
-    public static function hKeys(string $key) : array
-    {
-        return self::$client->hkeys($key);
-    }
-
-    /**
-     * 获取hash的所有字段值
-     * @param string $key
-     * @return array
-     */
-    public static function hValues(string $key) : array
-    {
-        return self::$client->hvals($key);
-    }
-
-    /**
-     * hash某一个字段自增
+     * 判断hash字段是否存在
      * @param string $key
      * @param string $field
-     * @param int $step
-     * @return int
+     * @return bool
      */
-    public static function hIncrBy(string $key, string $field, int $step = 1) : int
+    public static function hExists(string $key, string $field): bool
     {
-        return self::$client->hincrby($key, $field, $step);
+        $client = self::getClient();
+        return (bool)$client->hexists($key, $field);
     }
 
     /**
-     * hash某一个字段按照float自增
+     * 获取hash字段值并增加
      * @param string $key
      * @param string $field
-     * @param int|float $step
+     * @param int $increment
+     * @return int
+     */
+    public static function hIncrBy(string $key, string $field, int $increment): int
+    {
+        $client = self::getClient();
+        return (int)$client->hincrby($key, $field, $increment);
+    }
+
+    /**
+     * 获取hash字段值并增加
+     * @param string $key
+     * @param string $field
+     * @param float $increment
      * @return string
      */
-    public static function hIncrByFloat(string $key, string $field, int|float $step = 1) : string
+    public static function hIncrByFloat(string $key, string $field, float $increment): string
     {
-        return self::$client->hincrbyfloat($key, $field, $step);
+        $client = self::getClient();
+        return (string)$client->hincrbyfloat($key, $field, $increment);
     }
 
     /**
-     * hash设置某个字段的值，成功返回1，否则返回0
+     * 设置hash字段值（仅当字段不存在时）
      * @param string $key
      * @param string $field
      * @param string $value
-     * @return int
+     * @return bool
      */
-    public static function hSetNx(string $key, string $field, string $value) : int
+    public static function hSetNx(string $key, string $field, string $value): bool
     {
-        return self::$client->hsetnx($key, $field, $value);
+        $client = self::getClient();
+        return (bool)$client->hsetnx($key, $field, $value);
     }
 }
